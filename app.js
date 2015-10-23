f9d94a17e9e0823c7b0cd9c7ef9354dbb54b3755
Blake Guilloud <fbguillo@gmail.com>
2015-10-23 19:39:17 -0400

    another commit

---
+++ app.js
@@ -0,0 +1,147 @@
+$(document).ready(function(){
+
+
+
+
+///////////////////////// TEMPLATES ///////////////////////////
+
+
+  var albumTmpl = _.template($('#albumCoverTemplate').html());
+
+  albumHTML = "";
+  _.each(albumsData, function(currVal, idx, arr){
+    albumHTML += albumTmpl(currVal);
+  });
+    $('.albumContainer').html(albumHTML);
+
+  var buttonTmpl = _.template($('#buttonTemplate').html());
+
+  buttonHTML = "";
+  _.each(albumsData, function(currVal){
+    buttonHTML += buttonTmpl(currVal);
+  });
+    $('.button').html(buttonHTML);
+
+  $('.albumRef').on('click', function(event){
+    event.preventDefault();
+    clickedAlbum = "." + $(this).attr('class')
+    selectedAlbum = "." + $(this).attr('rel')
+    console.log("Rel Num", selectedAlbum);
+    $(this).parent('').removeClass('active');
+    $('.albumContainer').addClass('hidden');
+    // $('.photoDisplay').html(selectedAlbum);
+    $(selectedAlbum).removeClass('hidden');
+    $('nav').toggleClass('hidden');
+    $('#navButton').removeClass('hidden');
+    console.log(this)
+  });
+
+
+
+////////////////// PHOTO TEMPLATE NEEDS REFACTOR ///////////////// BUT FUCK THAT
+  var photoTmpl = _.template($('#albumTemplate').html());
+  photoHTML = "";
+  _.each(album1, function(currVal, idx, arr){
+    photoHTML += photoTmpl(currVal)
+
+  });
+  $('.album1').append(photoHTML)
+  photoHTML = "";
+  _.each(album2, function(currVal, idx, arr){
+    photoHTML += photoTmpl(currVal)
+  });
+  $('.album2').append(photoHTML)
+  photoHTML = "";
+  _.each(album3, function(currVal, idx, arr){
+    photoHTML += photoTmpl(currVal)
+  });
+  $('.album3').append(photoHTML)
+  photoHTML = "";
+  _.each(album4, function(currVal, idx, arr){
+    photoHTML += photoTmpl(currVal)
+  });
+  $('.album4').append(photoHTML)
+  photoHTML = "";
+  _.each(album5, function(currVal, idx, arr){
+    photoHTML += photoTmpl(currVal)
+  });
+  $('.album5').append(photoHTML)
+  photoHTML = "";
+  _.each(album6, function(currVal, idx, arr){
+    photoHTML += photoTmpl(currVal)
+  });
+  $('.album6').append(photoHTML)
+
+
+  $('.specificAlbum').on('click', function(event){
+    event.preventDefault();
+    $(this).siblings('').toggleClass('hidden');
+    $(this).toggleClass('singlePic');
+    $('#navButton').removeClass('hidden');
+  });
+
+
+
+
+  ///////// BUTTONS ///////
+
+
+
+  });
+
+
+  // var photoTmpl = _.template($('#albumTemplate').html());
+  // console.log(photoTmpl);
+  // window.photoHTML = "";
+  // _.each(photoData, function(currVal, idx, arr){
+  //   _.each(currVal, function(photbum) {
+  //     _.each(photbum, function(el) {
+  //       photoHTML = photoTmpl(el);
+  //       $('.photoDisplay').append(photoHTML)
+  //     })
+  //   })
+  // });
+
+
+
+//
+  //
+  //
+  // var photoTmpl = _.template($('#albumTemplate').html());
+  //
+  // photoHTML = "";
+  // singleAlbum = "";
+  // anotherDec = "";
+  //   _.each(album1, function( currVal, idx, arr){
+  //       photoHTML += photoTmpl ;
+  //       _.each(currVal, function(el, idx, arr){
+  //         singleAlbum += el.relNum;
+  //         console.log(el)
+  //       })
+  //   });
+
+
+
+/////////////////// ON CLICK ALBUM COVER ////////////////////
+
+
+
+
+//
+//
+//
+// $(document).ready(function () {
+//   // anything in here is dom ready!
+//  var bitterTemplate = _.template($('#bitterTmpl').html());
+//  console.log("bitter template: ", bitterTemplate);
+//   window.bitterHTML = "";
+//
+// // we need to merge our data from bitterData with actual markup so that we can add to our html document
+//   _.each(bitterData, function (currVal, idx, arr) {
+//     bitterHTML += bitterTemplate(currVal);
+//       // console.log('bitterHTML: ', bitterHTML);
+//   });
+// // add giant string of markup to the DOM
+// $('.bitters').html(bitterHTML);
+//
+// };
---
+++ data.js
@@ -0,0 +1,272 @@
+var albumsData = [
+{
+  relNumber: 0,
+  albumId: 'album1',
+  albumCover: "http://www.fillmurray.com/400/400",
+  albumName: 'Album 1'
+},
+{
+relNumber: 1,
+albumId: 'album2',
+albumCover: "http://www.fillmurray.com/400/400",
+albumName: 'Album 2'
+},
+{
+relNumber: 2,
+albumId: 'album3',
+albumCover: "http://www.fillmurray.com/400/400",
+albumName: 'Album 3'
+},
+{
+  relNumber: 3,
+albumId: 'album4',
+albumCover: "http://www.fillmurray.com/400/400",
+albumName: 'Album 4'
+},
+{
+  relNumber: 4,
+albumId: 'album5',
+albumCover: "http://www.fillmurray.com/400/400",
+albumName: 'Album 5'
+},
+{
+  relNumber: 5,
+albumId: 'album6',
+albumCover: "http://www.fillmurray.com/400/400",
+albumName: 'Album 6'
+},
+];
+
+
+
+
+var album1 =  [
+  {
+    relNum: 0,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 1',
+    photoDescription: 'Here is a picture of Bill Murray.'
+  },
+  {
+    relNum: 1,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 2',
+    photoDescription: 'Here is the 2nd picture of Bill Murray.'
+  },
+  {
+    relNum: 2,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 3',
+    photoDescription: 'Here is the 3rd picture of Bill Murray.'
+  },
+  {
+    relNum: 3,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 4',
+    photoDescription: 'Here is the 4th picture of Bill Murray.'
+  },
+  {
+    relNum: 4,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 5',
+    photoDescription: 'Here is the 5th picture of Bill Murray.'
+  },
+  {
+    relNum: 5,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 6',
+    photoDescription: 'Here is the last picture of Bill Murray.'
+  },
+]
+var album2 =  [
+  {
+    relNum: 0,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 1',
+    photoDescription: 'Here is a picture of Bill Murray.'
+  },
+  {
+    relNum: 1,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 2',
+    photoDescription: 'Here is the 2nd picture of Bill Murray.'
+  },
+  {
+    relNum: 2,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 3',
+    photoDescription: 'Here is the 3rd picture of Bill Murray.'
+  },
+  {
+    relNum: 3,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 4',
+    photoDescription: 'Here is the 4th picture of Bill Murray.'
+  },
+  {
+    relNum: 4,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 5',
+    photoDescription: 'Here is the 5th picture of Bill Murray.'
+  },
+  {
+    relNum: 5,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 6',
+    photoDescription: 'Here is the last picture of Bill Murray.'
+  },
+]
+
+var album3 =  [
+  {
+    relNum: 0,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 1',
+    photoDescription: 'Here is a picture of Bill Murray.'
+  },
+  {
+    relNum: 1,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 2',
+    photoDescription: 'Here is the 2nd picture of Bill Murray.'
+  },
+  {
+    relNum: 2,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 3',
+    photoDescription: 'Here is the 3rd picture of Bill Murray.'
+  },
+  {
+    relNum: 3,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 4',
+    photoDescription: 'Here is the 4th picture of Bill Murray.'
+  },
+  {
+    relNum: 4,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 5',
+    photoDescription: 'Here is the 5th picture of Bill Murray.'
+  },
+  {
+    relNum: 5,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 6',
+    photoDescription: 'Here is the last picture of Bill Murray.'
+  },
+]
+
+var album4 =  [
+  {
+    relNum: 0,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 1',
+    photoDescription: 'Here is a picture of Bill Murray.'
+  },
+  {
+    relNum: 1,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 2',
+    photoDescription: 'Here is the 2nd picture of Bill Murray.'
+  },
+  {
+    relNum: 2,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 3',
+    photoDescription: 'Here is the 3rd picture of Bill Murray.'
+  },
+  {
+    relNum: 3,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 4',
+    photoDescription: 'Here is the 4th picture of Bill Murray.'
+  },
+  {
+    relNum: 4,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 5',
+    photoDescription: 'Here is the 5th picture of Bill Murray.'
+  },
+  {
+    relNum: 5,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 6',
+    photoDescription: 'Here is the last picture of Bill Murray.'
+  },
+]
+var album5 =  [
+  {
+    relNum: 0,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 1',
+    photoDescription: 'Here is a picture of Bill Murray.'
+  },
+  {
+    relNum: 1,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 2',
+    photoDescription: 'Here is the 2nd picture of Bill Murray.'
+  },
+  {
+    relNum: 2,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 3',
+    photoDescription: 'Here is the 3rd picture of Bill Murray.'
+  },
+  {
+    relNum: 3,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 4',
+    photoDescription: 'Here is the 4th picture of Bill Murray.'
+  },
+  {
+    relNum: 4,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 5',
+    photoDescription: 'Here is the 5th picture of Bill Murray.'
+  },
+  {
+    relNum: 5,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 6',
+    photoDescription: 'Here is the last picture of Bill Murray.'
+  },
+]
+var album6 =  [
+  {
+    relNum: 0,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 1',
+    photoDescription: 'Here is a picture of Bill Murray.'
+  },
+  {
+    relNum: 1,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 2',
+    photoDescription: 'Here is the 2nd picture of Bill Murray.'
+  },
+  {
+    relNum: 2,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 3',
+    photoDescription: 'Here is the 3rd picture of Bill Murray.'
+  },
+  {
+    relNum: 3,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 4',
+    photoDescription: 'Here is the 4th picture of Bill Murray.'
+  },
+  {
+    relNum: 4,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 5',
+    photoDescription: 'Here is the 5th picture of Bill Murray.'
+  },
+  {
+    relNum: 5,
+    photoCover: 'http://www.fillmurray.com/200/300',
+    photoName: 'Bill Murray 6',
+    photoDescription: 'Here is the last picture of Bill Murray.'
+  },
+]
